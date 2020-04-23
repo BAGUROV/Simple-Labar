fba9b6210e95dc49f8068dc850cb0df3cf062ad2
Leonid Bagurov <leonid.1904.ghost@mail.ru>
2020-04-23 13:04:16 +0300

    Вместо умножения теперь деление

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
 #include "sdt.h"
 int main()//Программа является учебной 
 {
 	cout << "Enter: A and B";
 	int a, b;
 	cin >> a >>b;
-	cout << "A+B"+(a+b);
-	cout << "A-B" + (a - b);
+	cout << "A+B" + (a + b) << '\n'
+		 << "A-B" + (a - b) << '\n'
+		 << "A/B" + (a / b);
 	return 0;
-=======
-#include "sdt.h"
-int main()
-{
-	cout << "Enter: A and B";
-	int a, b;
-	cin >> a >>b;
-	cout << "A+B"+(a+b);
-	cout << "A-B" + (a - b);
-	return 0;
->>>>>>> bc2625385881366225c0f5cbb99a548bb5db89dc
 }
